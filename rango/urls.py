57e46bb807056c12cfd13eac66b6e6f305faa073
Yicheng Wang <2721840w@student.gla.ac.uk>
2023-02-08 17:31:21 +0000

    thirteenth commit

--- rango/urls.py
+++ rango/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf.urls import url
 from rango import views
 
 app_name = 'rango'
@@ -6,8 +7,9 @@ app_name = 'rango'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('category/<slug:category_name_slug>/add_page/',
+        views.add_page, name='add_page'),
     path('category/<slug:category_name_slug>/', views.show_category,
           name='show_category'),
     path('add_category/', views.add_category, name='add_category'),
-    path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),
 ]
