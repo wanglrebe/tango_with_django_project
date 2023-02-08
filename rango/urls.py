eac9271ee02dd44c10c447a5c6d893e92307502e
Yicheng Wang <2721840w@student.gla.ac.uk>
2023-02-08 23:34:15 +0000

    fifteenth commit

--- rango/urls.py
+++ rango/urls.py
@@ -13,5 +13,7 @@ urlpatterns = [
     name='show_category'),
     path('add_category/', views.add_category, name='add_category'),
     path('register/', views.register, name='register'), # New mapping!
+    path('login/', views.user_login, name='login'),
+
 ]
 
